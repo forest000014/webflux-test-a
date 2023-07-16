185364c0613c11dafcd889113178f64f3767e2ca
forest000014 <forest000014@gmail.com>
2023-07-16 20:44:18 +0900

    first commit

---
+++ src/main/java/com/forest000014/webfluxtesta/WebfluxTestAApplication.java
@@ -0,0 +1,13 @@
+package com.forest000014.webfluxtesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxTestAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxTestAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forest000014/webfluxtesta/WebfluxTestAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forest000014.webfluxtesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxTestAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
